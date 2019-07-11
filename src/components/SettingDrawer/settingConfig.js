c2ed475c5e3c3cdb36e7173d6852a8582d78bd85
luochen <tsukimoriyoko@yeah.net>
2019-07-11 18:05:18 +0800

    :construction: Basic router

--- src/components/SettingDrawer/settingConfig.js
+++ src/components/SettingDrawer/settingConfig.js
@@ -21,7 +21,7 @@ const colorList = [
     key: '极光绿', color: '#52C41A'
   },
   {
-    key: '拂晓蓝（默认）', color: '#1890FF'
+    key: '拂晓蓝（默认）', color: '#722ED1'
   },
   {
     key: '极客蓝', color: '#2F54EB'
